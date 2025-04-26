d86b17dc7e6d5c385056f7d51cdd495b9e2aad78
juliaturness <jmturnes10@gmail.com>
2025-04-25 22:10:41 -0300

    começando a fila sequencial simples

---
+++ src/esd/ListaSequencialSimples.java
@@ -0,0 +1,77 @@
+package esd;
+
+
+public class ListaSequencialSimples <T> {
+
+    T[] area;
+    int len = 0;
+    int fim = 0;
+    final int defcap = 8;
+
+    @SuppressWarnings("unchecked")
+    public ListaSequencialSimples() {
+        area = (T[])new Object[defcap];
+    }
+
+    @SuppressWarnings("unchecked")
+    void expande(int len) {
+        // isto será usado quando for necessário expandir a capacidade da lista
+    }
+
+
+    public void expande() {
+        // expande a capacidade da lista: nova capacidade deve ser o dobro da atual
+        expande(2*area.length);
+    }
+
+    public boolean esta_vazia() {
+        return len == 0;
+    }
+
+    public int capacidade() {
+        return len ;
+    }
+
+    public void adiciona(T elemento) {
+        if (len == area.length){
+            expande();
+        }
+        area[fim] = elemento;
+        len ++;
+        fim = (fim + 1) / area.length;
+
+    }
+
+    public void remove(int indice) {
+        // remove um valor da posição indicada pelo parâmetro "indice"
+        // move para essa posição o valor que está no final da lista
+        // disparar uma exceção IndexOutOfBoundsException caso posição seja inválida
+
+
+    }
+
+    public int procura(T valor) {
+        // retorna um inteiro que representa aposição onde valor foi encontrado pela primeira vez (contando do início da lista)
+        // retorna -1 se não o encontrar !
+    }
+
+    public T obtem(int indice) {
+        // retorna o valor armazenado na posição indica pelo parâmetro "indice"
+        // disparar uma exceção IndexOutOfBoundsException caso posição seja inválida
+    }
+
+    public void substitui(int indice, T valor) {
+        // armazena o valor na posição indicada por "indice", substituindo o valor lá armazenado atualmente
+        // disparar uma exceção IndexOutOfBoundsException caso posição seja inválida
+    }
+
+    public int comprimento() {
+        // retorna um inteiro que representa o comprimento da lista (quantos valores estão armazenados)
+        return area.length;
+    }
+
+    public void limpa() {
+
+        // esvazia a lista
+    }
+}
---
+++ test/TestListaSequencialSimples.java
@@ -0,0 +1,180 @@
+//import esd.ListaSequencial;
+import esd.ListaSequencialSimples;
+import org.junit.jupiter.api.DisplayName;
+
+import java.io.IOException;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class TestListaSequencialSimples {
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Testa criação de lista: deve estar vazia ao ser criada")
+    void criaListaSequencialSimples() throws InterruptedException, IOException {
+        ListaSequencialSimples<Integer> q = new ListaSequencialSimples<>();
+        assert(q.esta_vazia());
+        assert(q.comprimento() == 0);
+        assert(q.capacidade() > 0);
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Testa obter valor de posição inválida")
+    void obtem_invalido() throws InterruptedException, IOException {
+        ListaSequencialSimples<Integer> q = new ListaSequencialSimples<>();
+
+        // acessar uma posição em lista vazia
+        assertThrows(IndexOutOfBoundsException.class, () -> {q.obtem(0);});
+        q.adiciona(Integer.valueOf(10));
+
+        // acessar uma posição igual ao comprimento da lista
+        assertThrows(IndexOutOfBoundsException.class, () -> {q.obtem(1);});
+        // acessar uma posição maior que comprimento da lista
+        assertThrows(IndexOutOfBoundsException.class, () -> {q.obtem(7);});
+    }
+
+    ListaSequencialSimples gera_lista() {
+        ListaSequencialSimples<Integer> q = new ListaSequencialSimples<>();
+
+        for (int j=0; j < q.capacidade(); j++) {
+            var val = Integer.valueOf(j);
+            q.adiciona(val);
+        }
+
+        return q;
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Testa anexar e obter valores")
+    void adiciona() throws InterruptedException, IOException {
+        ListaSequencialSimples<Integer> q = new ListaSequencialSimples<>();
+
+        // gera uma lista com inteiros crescentes
+        for (int j=0; j < q.capacidade(); j++) {
+            var val = Integer.valueOf(j);
+            q.adiciona(val);
+            // verifica se comprimento correspodne à quantidade de valores adicionados
+            assert(q.comprimento() == j+1);
+        }
+
+        // verifica se a lista contém os valoers na ordem em que foram adicionados
+        for (int j=0; j < q.capacidade(); j++) {
+            var val = Integer.valueOf(j);
+            assert(val.equals(q.obtem(j)));
+        }
+
+        // confere se a lista não está vazia
+        assert(! q.esta_vazia());
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Testa remover valores do inicio ou do meio da lista")
+    void remove_do_meio() throws InterruptedException, IOException {
+        ListaSequencialSimples<Integer> q = gera_lista();
+
+        // remove valores de posições aleatórias
+        Random rand = new Random();
+        for (int j=q.comprimento()-1; j > 0; j--) {
+            // lista ainda não pode estar vazia
+            assert(! q.esta_vazia());
+            // sorteia uma posição de onde remover um valor
+            int pos = rand.nextInt(q.comprimento()-1);
+            // copia o valor que está no final da lista
+            Integer val = q.obtem(q.comprimento()-1);
+            q.remove(pos);
+            // verifica se o valor na posição após a remoção é o mesmo que estava no final da lista
+            Integer val2 = q.obtem(pos);
+            assert(val.equals(val2));
+        }
+
+        // confere se o comprimento é consistente com a quantidade de remoções
+        assert(q.comprimento() == 1);
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Testa remover valor de posição inválida")
+    void remove_invalido() throws InterruptedException, IOException {
+        ListaSequencialSimples<Integer> q = new ListaSequencialSimples<>();
+
+        // remover de lista vazia
+        assertThrows(IndexOutOfBoundsException.class, () -> {q.remove(0);});
+        q.adiciona(Integer.valueOf(10));
+        // remover da posição igual ao comprimento
+        assertThrows(IndexOutOfBoundsException.class, () -> {q.remove(1);});
+        // remover da posição maior que comprimento
+        assertThrows(IndexOutOfBoundsException.class, () -> {q.remove(7);});
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Testa remover valores do fim da lista")
+    void remove_do_fim() throws InterruptedException, IOException {
+        ListaSequencialSimples<Integer> q = gera_lista();
+
+        // remove valor do fim da lista repetidas vezes
+        for (int j=q.comprimento()-1; j > 0; j--) {
+            assert(! q.esta_vazia());
+            q.remove(j);
+            // verifica se comprimento foi reduzido
+            assert(q.comprimento() == j);
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Testa procurar valores na lista")
+    void procura() throws InterruptedException, IOException {
+        ListaSequencialSimples<Integer> q = new ListaSequencialSimples<>();
+
+        // procura em lista vazia
+        assert(q.procura(Integer.valueOf(3)) == -1);
+
+        q = gera_lista();
+
+        for (int j=0; j < q.capacidade(); j++) {
+            var val = Integer.valueOf(j);
+            // verifica se a posição o valor ptocurado está correta
+            // note que cada posição contém um valor igual ao número dessa posição (ver gera_lista())
+            int indice = q.procura(j);
+            assert(indice == j);
+        }
+
+        // procura um valor que não existe numa lista não-vazia
+        assert(q.procura(Integer.valueOf(1001)) == -1);
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Testa limpar uma lista")
+    void limpa() throws InterruptedException, IOException {
+        ListaSequencialSimples<Integer> q = new ListaSequencialSimples<>();
+
+        q.limpa();
+        assert(q.comprimento() == 0);
+        assert(q.esta_vazia());
+
+        q = gera_lista();
+        q.limpa();
+        assert(q.comprimento() == 0);
+        assert(q.esta_vazia());
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Testa substituir valores")
+    void substitui() throws InterruptedException, IOException {
+        ListaSequencialSimples<Integer> q = gera_lista();
+
+        Random rand = new Random();
+        for (int j=q.comprimento(); j > 1 ; j--) {
+            assert(! q.esta_vazia());
+            // sorteia uma posição para substituição
+            int pos = rand.nextInt(q.comprimento()-1);
+            // obtém o valor dessa posição, e soma 100 a ele para gerar um novo valor
+            Integer val = q.obtem(pos) + 100;
+            // substitui o valor da posição pelo novo valor
+            q.substitui(pos, val);
+            // confere se ovalor atual na posição é o novo valor lá substituido
+            var val2 = q.obtem(pos);
+            assert(val.equals(val2));
+        }
+
+    }
+
+}
